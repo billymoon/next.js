7343c29389d189421b6c27a4d3b5617f75a3062a
Billy Moon <billy.moon@bjss.com>
2024-08-02 10:52:47 +0100

    reproduce the problem

--- examples/with-redux/app/verify/page.tsx
+++ examples/with-redux/app/verify/page.tsx
@@ -1,4 +1,18 @@
+"use client";
+import { increment } from "@/lib/features/counter/counterSlice";
+import { useAppStore } from "@/lib/hooks";
+import { useRef } from "react";
+
 export default function VerifyPage() {
+  // set store data as described in...
+  // https://redux.js.org/usage/nextjs#per-route-state
+  const store = useAppStore();
+  const initialized = useRef(false);
+  if (!initialized.current) {
+    store.dispatch(increment());
+    initialized.current = true;
+  }
+
   return (
     <>
       <h1>Verify page</h1>
